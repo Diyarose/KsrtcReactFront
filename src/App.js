b8504551b82e8e8895d9e8e8c90fe680d1571d54
Diyarose <dhiyarosedavis@gmail.com>
2024-06-13 22:46:56 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginBus from './components/LoginBus';
+import SignupBus from './components/SignupBus';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <LoginBus/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<LoginBus/>}/>
+    <Route path='/signup' element={<SignupBus/>}/>
+
+   </Routes>
+   </BrowserRouter>
   );
 }
 
--- src/components/LoginBus.jsx
+++ src/components/LoginBus.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginBus = () => {
+    const [data,setData]=useState(
+        {
+           
+            "email":"",
+            
+            "pass":""
+            
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        
+                console.log(data)
+                axios.post("http://localhost:8080/login",data).then(
+                    (response)=>{
+                        console.log(response.data)
+                        if(response.data.status=="success")
+                            {
+                                alert("Successfully added")
+                            }
+                        else{
+                            alert("Error")
+                        }
+                    }
+                ).catch(
+                    (error)=>{
+                        console.log(error.message)
+                    }
+                ).finally()
+            
+       
+    }
   return (
     <div>
         <div className="container">
@@ -8,14 +44,17 @@ const LoginBus = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label htmlFor="" className="form-label">Email</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label htmlFor="" className="form-label">Password</label>
-                    <input type="password" name="" id="" className="form-control" />
+                    <input type="password"  id="" className="form-control" name='pass' value={data.pass} onChange={inputHandler} />
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-success">Login</button>
+                    <button className="btn btn-success" onClick={readValue}>Login</button>
+                </div>
+                <div>
+                <Link class="nav-link" to="/signup">SIGNUP</Link>
                 </div>
             </div>
         </div>
